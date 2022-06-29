00c819bfc4a07cdceef479136aa0d1e3b7d8957a
Andras Zolyomi <azolyomi@andrass-mbp-2.lan>
2022-06-28 22:53:21 -0600

    add modrole, staffrole, vcless_organizer_role permissions for future use

--- src/permissions.py
+++ src/permissions.py
@@ -13,3 +13,24 @@ def has_admin_role(ctx):
         if role.id in ServerConfigs[ctx.guild.id]['adminroles']:
             return True
     return False
+
+def has_mod_role(ctx):
+    if not has_config(ctx): return False
+    for role in ctx.author.roles:
+        if role.id in ServerConfigs[ctx.guild.id]['modroles']:
+            return True
+    return False
+
+def has_staff_role(ctx):
+    if not has_config(ctx): return False
+    for role in ctx.author.roles:
+        if role.id in ServerConfigs[ctx.guild.id]['staffroles']:
+            return True
+    return False
+
+def has_vcless_organizer_role(ctx, member, category):
+    if not has_config(ctx): return False
+    for role in member.roles:
+        if role.id in ServerConfigs[ctx.guild.id]['raiding']['vcless']['categories'][category]['roles']:
+            return True
+    return False
