be87418ce07b527d2cc070e34f6bcd48fd18813f
Andras Zolyomi <azolyomi@andrass-mbp-2.lan>
2022-06-30 00:26:46 -0600

    move messages into errors file

--- src/permissions/errors.py
+++ src/permissions/errors.py
@@ -1,21 +1,23 @@
-from discord.ext.commands import CommandError
+from discord.ext.commands import CommandError, NoPrivateMessage
+from util.constants import PREFIX
 
-# Global Error setup 
-class GlobalError(CommandError):
-    def __init__(self, error):
-        self.original = error
+ERROR_NOT_IN_GUILD = "This command can only be used in a server."
+ERROR_NOT_FOUND = "Command not found. Use `{}help` for a list of commands.".format(PREFIX)
+ERROR_NO_PERMISSION = "You do not have permission to use this command."
+ERROR_NOT_ADMIN = "You must have an `admin` role configured with the bot to use this command."
+ERROR_NOT_MOD = "You must have a `mod` role configured with the bot to use this command."
+ERROR_NOT_STAFF = "You must have a `staff` role configured with the bot to use this command."
 
-def propagate(ctx, error):
-    ctx.bot.dispatch('command_error', ctx, GlobalError(error))
+
 
 class StarshipPermissionsError(CommandError):
-    message = "You do not have permission to use this command."
+    message = ERROR_NO_PERMISSION
 
 class NotModError(StarshipPermissionsError):
-    message = "You must have a `mod` role configured with the bot use this command."
+    message = ERROR_NOT_MOD
 
 class NotAdminError(StarshipPermissionsError):
-    message = "You must have an `admin` role configured with the bot use this command."
+    message = ERROR_NOT_ADMIN
 
 class NotStaffError(StarshipPermissionsError):
-    message = "You must have a `staff` role configured with the bot use this command."
+    message = ERROR_NOT_STAFF
--- src/permissions/messages.py
+++
@@ -1,8 +0,0 @@
-from util.constants import PREFIX
-
-ERROR_NOT_FOUND = "Command not found. Use `{}help` for a list of commands.".format(PREFIX)
-ERROR_NOT_IN_GUILD = "This command can only be used in a server."
-ERROR_NOT_ADMIN = "You must have an `admin` role configured with the bot use this command."
-ERROR_NOT_MOD = "You must have a `mod` role configured with the bot use this command."
-ERROR_NOT_STAFF = "You must have a `staff` role configured with the bot use this command."
-ERROR_NO_PERMISSION = "You do not have permission to use this command."
