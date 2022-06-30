e38c582620c07cce06e4c1549ef52c13d832d34e
Andras Zolyomi <azolyomi@andrass-mbp-2.lan>
2022-06-29 19:51:34 -0600

    add patreon cog

--- src/bot.py
+++ src/bot.py
@@ -2,8 +2,10 @@ import discord
 from discord.ext import commands
 import logging
 import os
+from util.constants import PREFIX
 from database import db, ServerConfigs
 from dotenv import load_dotenv
+
 
 # load bot token and other .env data
 load_dotenv()
@@ -17,12 +19,14 @@ bot = commands.Bot(command_prefix='!')
 
 # cog registration
 bot.load_extension("cogs.help")
+bot.load_extension("cogs.patreon")
 bot.load_extension("cogs.ping")
 bot.load_extension("cogs.setup")
 bot.load_extension("cogs.config")
 
 @bot.event
 async def on_ready():
+    await bot.change_presence(activity=discord.Game(name=" {0}help | {0}patreon".format(PREFIX)))
     print('We have logged in as {0.user}'.format(bot))
     configs = list(db.ServerConfigs.find())
     for config in configs:
---
+++ src/cogs/patreon.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+from emoji import heart
+
+class Patreon(commands.Cog):
+    """Patreon cog to link users to buy me a coffee."""
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command(aliases=["owner", "coffee", "support", "pog", "pogchamp"])
+    async def patreon(self, ctx):
+        """Link to the patreon page and to buy me a coffee."""
+        embed = discord.Embed(
+            title="Support the Creator",
+            description="If you want to **support me**, please consider [buying me a coffee](https://www.buymeacoffee.com/Theurul)\n\nYou can also [subscribe to my patreon](https://patreon.com/theurul), where supporters are given access to **cool bot features** and unique server permissions in various communities. ",
+            color=discord.Color.orange()
+        )
+        embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/942320785287184464/eac00b47a7f6b7883d1260f8bb3111e6.png?size=4096")
+        embed.set_footer(text="Your local, friendly bot dev Theurul {}".format(heart))
+        await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Patreon(bot))
