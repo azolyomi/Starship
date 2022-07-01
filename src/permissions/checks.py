34f914f9ddd12d5c622473b3a45e5f9fbfa5fe7c
Andras Zolyomi <azolyomi@andrass-mbp-2.lan>
2022-07-01 14:58:46 -0600

    polymorphism to clean up permissions

--- src/permissions/checks.py
+++ src/permissions/checks.py
@@ -7,30 +7,44 @@ from permissions import errors
 def is_urul(ctx):
     return ctx.author.id == THEURUL_USER_ID
 
+# LOCAL UTIL FUNCS
+
+def is_admin(ctx, roleid): 
+    return roleid in ServerConfigs[ctx.guild.id]['adminroles']
+
+def is_mod(ctx, roleid):
+    return roleid in ServerConfigs[ctx.guild.id]['modroles']
+
+def is_staff(ctx, roleid):
+    return roleid in ServerConfigs[ctx.guild.id]['staffroles']
+
+# EXPORTED FUNCS
+
+# equivalent to guild_only() + check config exists
 def has_config(ctx):
     if (ctx.guild is None): 
         raise NoPrivateMessage()
     return (ServerConfigs[ctx.guild.id] is not None)
     
-
+# equivalent to guild_only() + check config exists + check admin role in config
 def has_admin_role(ctx):
     if not has_config(ctx): return False
     for role in ctx.author.roles:
-        if role.id in ServerConfigs[ctx.guild.id]['adminroles']:
+        if is_admin(ctx, role.id):
             return True
     raise errors.StarshipRoleMissingError("admin")
 
 def has_mod_role(ctx):
     if not has_config(ctx): return False
     for role in ctx.author.roles:
-        if role.id in ServerConfigs[ctx.guild.id]['modroles']:
+        if is_mod(ctx, role.id) or is_admin(ctx, role.id):
             return True
     raise errors.StarshipRoleMissingError("mod")
 
 def has_staff_role(ctx):
     if not has_config(ctx): return False
     for role in ctx.author.roles:
-        if role.id in ServerConfigs[ctx.guild.id]['staffroles']:
+        if is_staff(ctx, role.id) or is_admin(ctx, role.id):
             return True
     raise errors.StarshipRoleMissingError("staff")
 
--- src/permissions/errors.py
+++ src/permissions/errors.py
@@ -5,8 +5,10 @@ ERROR_NOT_IN_GUILD = "This command can only be used in a server."
 ERROR_NOT_FOUND = "Command not found. Use `{}help` for a list of commands.".format(PREFIX)
 ERROR_NO_PERMISSION = "You do not have permission to use this command."
 
+vowel = "aeiouy"
+
 def ERROR_MISSING_ROLE(role):
-    return "You must have an `{}` role configured with the bot to use this command.".format(role)
+    return "You must have a{} `{}` role configured with the bot to use this command.".format(('n' if role[0].lower() in vowel else ''), role)
 
 class StarshipPermissionsError(CommandError):
     message = ERROR_NO_PERMISSION
